174bc50dd60cd97fa6b86467b60414b934695e8b
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-06-15 22:47:10 -0700

    up

---
+++ client/src/main/java/com/rpg/client/Client.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rpg.client;
+
+/**
+ *
+ * @author fernando
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
